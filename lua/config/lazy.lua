2ae453701e9ae9e61a5e1c9946fbcf406cc635a6
Yelleyo <yelletouil@gmail.com>
2024-01-01 15:19:57 +0100

    lazy clonfig no auto checker

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,7 +18,7 @@ require("lazy").setup({
   },
 
   install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+
   performance = {
     rtp = {
       -- disable some rtp plugins
