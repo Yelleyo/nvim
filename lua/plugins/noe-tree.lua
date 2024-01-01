a03d21988e2294ed193b8a72c4984aeadcff0cbd
Yelleyo <yelletouil@gmail.com>
2024-01-01 14:48:40 +0100

    Neo-tree dependencies

--- lua/plugins/noe-tree.lua
+++ lua/plugins/noe-tree.lua
@@ -1,6 +1,11 @@
 
 return {
-  "nvim-neo-tree/neo-tree.nvim",
+    "nvim-neo-tree/neo-tree.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
     require("neo-tree").setup({
         window = {
           position = "right",
