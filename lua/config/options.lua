e5d8a4db45964c1097bcfdbdff62f2243143b3f0
Yelleyo <yelletouil@gmail.com>
2024-01-08 17:04:17 +0100

    reformat options.lua with GPT comment on each setting

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,48 +1,48 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+
 -- Add any additional options hereby
--- Disabling standard nvim explorer for nvim tree vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 1 -- Disables the netrw file explorer
+vim.g.loaded_netrwPlugin = 1 -- Disables the netrwPlugin
 
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" }) -- Sets the background color for normal text to none
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }) -- Sets the background color for floating text to none
+vim.opt.termguicolors = true -- Enables true color support
 
-vim.opt.clipboard = "unnamedplus" -- allows use of the system clipboard
-vim.opt.splitright = true
-vim.opt.ignorecase = true
+vim.opt.clipboard = "unnamedplus" -- Allows use of the system clipboard
+vim.opt.splitright = true -- Splits new windows to the right
 
-vim.g.autoformat = false
-vim.opt.nu = true
-vim.opt.relativenumber = true
+vim.g.autoformat = false -- Disables autoformat
+vim.opt.nu = true -- Enables line numbers
+vim.opt.relativenumber = true -- Shows line numbers relative to the cursor
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.tabstop = 4 -- Sets the width of a tab to 4 spaces
+vim.opt.softtabstop = 4 -- Sets the number of spaces a Tab counts for while performing editing operations
+vim.opt.shiftwidth = 4 -- Sets the number of spaces used for indentation
+vim.opt.expandtab = true -- Converts tabs to spaces
+vim.opt.smartindent = true -- Enables smart indentation
 
-vim.opt.smartindent = true
+vim.opt.wrap = false -- Disables line wrapping
 
-vim.opt.wrap = false
-
-vim.opt.swapfile = false
-vim.opt.backup = false
+vim.opt.swapfile = false -- Disables swap file creation
+vim.opt.backup = false -- Disables backup creation
 local home_dir = os.getenv("HOME") or os.getenv("USERPROFILE")
-vim.opt.undodir = home_dir .. "/.vim/undodir"
-vim.opt.undofile = true
+vim.opt.undodir = home_dir .. "/.vim/undodir" -- Sets the directory for undo files
+vim.opt.undofile = true -- Enables undo file usage
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+-- Options for the grep search
+vim.opt.hlsearch = false -- Disables highlighting of search results
+vim.opt.incsearch = true -- Enables incremental search
+vim.opt.ignorecase = true -- Ignores case in search patterns
 
-vim.opt.termguicolors = true
+vim.opt.scrolloff = 8 -- Keeps 8 lines above and below the cursor when scrolling
+vim.opt.signcolumn = "yes" -- Always shows the sign column
+vim.opt.isfname:append("@-@") -- Appends @-@ to the list of characters that can be part of a file name
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+vim.opt.updatetime = 50 -- Sets the time of no activity before the swap file is written to disk
 
-vim.opt.updatetime = 50
+vim.opt.colorcolumn = "0" -- Disables color column
 
-vim.opt.colorcolumn = "0"
-
-vim.opt.timeoutlen = 0
-vim.opt.ttimeoutlen = 20
-vim.opt.numberwidth = 1
+vim.opt.timeoutlen = 0 -- Sets the time to wait for a mapped sequence to complete
+vim.opt.ttimeoutlen = 20 -- Sets the time to wait for a key code sequence to complete
+vim.opt.numberwidth = 1 -- Sets the width of the number column to 1
