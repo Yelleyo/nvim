9985b926f0d8936dfdac472f70983b7a36262fa4
Yelleyo <yelletouil@gmail.com>
2024-02-24 15:25:45 +0100

    Smaller font for Neovide h12

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@
 require("config.lazy")
 require("config.options")
 
-vim.o.guifont = "Iosevka NF:h14"
+vim.o.guifont = "Iosevka NF:h12"
 
 vim.g.neovide_transparency = 0.95
 vim.api.nvim_set_keymap('n', '<F11>', ":let g:neovide_fullscreen = !g:neovide_fullscreen<CR>", {})
