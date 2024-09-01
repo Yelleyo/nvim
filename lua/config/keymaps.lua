15ea083fd5e8ae039394fa2d06ad29281ff0c080
Yelleyo <yelletouil@gmail.com>
2024-09-01 12:37:29 +0200

    Keymap fix

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,9 +5,9 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = ' '
 
 -- bind for :q, :w and :s
-vim.api.nvim_set_keymap('n', '<leader>qq', ':q<CR>', {desc = "Quit", noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>ww', ':w<CR>', {desc = "Write", noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>wo', ':so<CR>', {desc = "Source out", noremap = true, silent = true})
+vim.keymap.set('n', '<leader>qq', ':q<CR>', {desc = "Quit", noremap = true, silent = true})
+vim.keymap.set('n', '<leader>ww', ':w<CR>', {desc = "Write", noremap = true, silent = true})
+vim.keymap.set('n', '<leader>wo', ':so<CR>', {desc = "Source out", noremap = true, silent = true})
 
 -- Move selected lines down & up
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move lines down" })
@@ -37,11 +37,10 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>o", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace all same" })
 
 -- Moving between windows / honnestly Ctrl hjkl is also fine
-vim.api.nvim_set_keymap("n", "<Leader>wh", "<C-w>h", { desc = "Window left", noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>wl", "<C-w>l", { desc = "Window right", noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>wj", "<C-w>j", { desc = "Window down", noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>wk", "<C-w>k", { desc = "Window up", noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>ws", "<C-w>v", { desc = "Split window vertically", noremap = true })
+vim.keymap.set("n", "<Leader>wh", "<C-w>h", { desc = "Window left", noremap = true })
+vim.keymap.set("n", "<Leader>wl", "<C-w>l", { desc = "Window right", noremap = true })
+vim.keymap.set("n", "<Leader>wk", "<C-w>k", { desc = "Window up", noremap = true })
+vim.keymap.set("n", "<Leader>ws", "<C-w>v", { desc = "Split window vertically", noremap = true })
 
 vim.keymap.set('n', '<leader>wu', vim.cmd.UndotreeToggle, {desc = "Toggle UndoTree", noremap = true })
 
