16a551c6254d5f21543fa0ec54655c4abfbc0172
Yellet <y.touil@topdesk.com>
2024-09-22 23:03:35 +0200

    Adding TransparentEnable to the autocmds.lua

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,3 +3,6 @@
 -- Add any additional autocmds here
 vim.cmd[[highlight LineNr guifg=#4C566A]] -- Color of the wind, and the line numbering
 vim.cmd[[highlight CursorLineNr guifg=#97B67C]] -- Color of the current line - taken from the nordic theme: https://www.nordtheme.com/
+vim.api.nvim_create_autocmd("VimEnter", {
+    command = "TransparentEnable"
+})
